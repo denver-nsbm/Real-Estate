1b05ac89414f5b31a5ba65cd30fe86fd41420850
warapitiya <warapitiya93@gmail>
2015-04-02 00:59:22 +0530

    Add Sorted List class with an overloaded constructor

---
+++ src/com/denver/citiestate/utils/SortedList.java
@@ -0,0 +1,16 @@
+package com.denver.citiestate.utils;
+
+import com.denver.citiestate.interfaces.Listable;
+import com.denver.citiestate.pojo.ListHouse;
+
+public class SortedList extends List {
+
+	/**
+	 * 
+	 * @param maxSpace
+	 */
+	public SortedList(int maxSpace) {
+		super(maxSpace);
+	}
+
+}
